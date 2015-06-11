56b6530baa1ef165e191ef440bff4b30cb64f8c1
Katarzyna Stepek <kstepek@hotmail.com>
2015-06-12 00:23:41 +0200

    Fasta DAG class and fuctions

--- modules/Fasta.py
+++ modules/Fasta.py
@@ -18,6 +18,66 @@ k=2
 cutoff=10 
 
 
+class ShortestPathResult(object):
+	def __init__(self):
+                self.d = {}
+                self.parent = {}
+
+def shortest_path(graph, s):
+        """Single source shortest paths using DP on a DAG.
+        Args:
+        graph: weighted DAG.
+        s: source
+        """
+        result = ShortestPathResult()
+        result.d[s] = 0
+
+        result.parent[s] = None
+        for v in graph.itervertices():
+                result=sp_dp(graph, v, result)
+        return result
+
+def sp_dp(graph, v, result):
+        """Recursion on finding the shortest path to v.
+         Args:
+                 graph: weighted DAG.
+                 v: a vertex in graph.
+                 result: for memoization and keeping track of the result.
+        """
+        if v in result.d:
+                return result.d[v]
+        result.d[v] = float('inf')
+        result.parent[v] = None
+        for u in graph.inverse_neighbors(v): # Theta(indegree(v))
+                new_distance = sp_dp(graph, u, result) + graph.weight(u, v)
+                if new_distance < result.d[v]:
+                        result.d[v] = new_distance
+                        result.parent[v] = unic
+        return result.d[v]
+
+
+def shortest_path_bottomup(graph, s):
+        """Bottom-up DP for finding single source shortest paths on a DAG.
+        Args:
+                graph: weighted DAG.
+                s: source
+        """
+        order = topological_sort(graph)
+        result = ShortestPathResult()
+        for v in graph.itervertices():
+                result.d[v] = float('inf')
+                result.parent[v] = None
+        result.d[s] = 0
+        for v in order:
+                for w in graph.neighbors(v):
+                        new_distance = result.d[v] + graph.weight(v, w)
+                        if result.d[w] > new_distance:
+                                result.d[w] = new_distance
+                                result.parent[w] = vars
+        return result
+
+
+
 def getTuplesList(str):
 	tuples=set()
 	tuplesDict={}
