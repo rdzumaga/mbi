ed2e13bb74dc1df1410fcadf68bf778f7cb8b711
ewelinag <grudzien.e@gmail.com>
2015-06-12 01:18:23 +0200

    wyswietlanie tabelki krokowo

--- static/js/algorithms.js
+++ static/js/algorithms.js
@@ -120,7 +120,16 @@ function addValueToMatrix(){
     for(i=1; i<=iters; i++){
         var id = "values" + i;
         var val = document.getElementById(id).value;
+        var arrayVal = getArray(val);
+        //var x = arrayVal.split(", ");
         var cellId = "cell" + i;
-        document.getElementById(cellId).innerText = val;
+       // document.getElementById(cellId).innerText = val;
+         document.getElementById(cellId).innerHTML = arrayVal[0];
     }
+}
+
+function getArray(val){
+    var str = val.substring(1, val.length-1);
+    var array = str.split(", ");
+        return array;
 }
