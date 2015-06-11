4d2d689ceb5272293af9e2c0494545dd1c23ec2d
ewelinag <grudzien.e@gmail.com>
2015-06-11 16:24:51 +0200

    walidacje

--- controllers/default.py
+++ controllers/default.py
@@ -14,9 +14,9 @@ def algorithm():
 
     d = ""
 
-    form=FORM(DIV(LABEL('Sekwencja A:', _for="seqA", _class='col-sm-2 control-label'), DIV(INPUT(_id='seqA', _name='seqA', _type='text', requires = IS_ALPHANUMERIC(error_message='Musisz podać AGCT'), _class='form-control', _placeholder = 'np. AAGCT'), _class='col-md-4'), _class='form-group', _id='seqADiv'),
+    form=FORM(DIV(LABEL('Sekwencja A:', _for="seqA", _class='col-sm-2 control-label'), DIV(INPUT(_id='seqA', _name='seqA', _type='text', _style='text-transform: uppercase', requires = IS_ALPHANUMERIC(error_message='Musisz podać AGCT'), _class='form-control', _placeholder = 'np. AAGCT'), _class='col-md-4'), _class='form-group', _id='seqADiv'),
               
-              DIV(LABEL('Sekwencja B:', _for="seqB", _class='col-sm-2 control-label'), DIV(INPUT(_id='seqB', _name='seqB', _type='text', requires = [IS_ALPHANUMERIC(error_message='Musisz podać AGCT')], _class='form-control', _placeholder = 'np. AAGCT'), _class='col-md-4'), _class='form-group',  _id='seqBDiv'),
+              DIV(LABEL('Sekwencja B:', _for="seqB", _class='col-sm-2 control-label'), DIV(INPUT(_id='seqB', _name='seqB', _type='text', _style='text-transform: uppercase', requires = [IS_ALPHANUMERIC(error_message='Musisz podać AGCT')], _class='form-control', _placeholder = 'np. AAGCT'), _class='col-md-4'), _class='form-group',  _id='seqBDiv'),
               
               DIV(LABEL('Kara za przerwę:', _for="break_penalty", _class='col-sm-2 control-label'), DIV(INPUT(_id='break_penalty', _name='break_penalty', requires=IS_INT_IN_RANGE(-10, 11,error_message='Musisz podać liczbę'), _class='form-control', _placeholder = 'np. -2'), _class='col-md-4'), _class='form-group'),
               
--- static/js/algorithms.js
+++ static/js/algorithms.js
@@ -17,32 +17,56 @@ function sequenceValidation() {
     var seqA = document.getElementById("seqA");
     var seqB = document.getElementById("seqB");
     var ok = true;
-    if (!seqA.value.match("A|C|T|G")) {
-        seqA.className = "nonValidate";
+    if (!seqA.value.toUpperCase().match("ACTG")) {
+        seqA.className = "form-control nonValidate";
         addWarning('A');
         ok = false;
     }
-    if (!seqB.value.match("A|C|T|G")) {
-        seqB.className = "nonValidate";
+    else {
+        removeWarning('A');
+    }
+    if (!seqB.value.toUpperCase().match("ACTG")) {
+        seqB.className = "form-control nonValidate";
         addWarning('B');
         ok = false;
+    }
+    else {
+        removeWarning('B');
     }
     return ok;
 }
 
 function addWarning(seq) {
-    var div = document.createElement('div');
-    div.className = 'error';
-    if (seq == 'A') {
-        div.innerHTML = '<div class="error" id="break_penalty__error" style="display: inline-block;">Sekwencja A jest nieprawidłowa! Musi składać się wyłącznie ze znaków A,C,T,G i nie może być pusta</div>';
-        document.getElementById('seqADiv').appendChild(div);
+    var errorA = document.getElementById('errorA');
+    var errorB = document.getElementById('errorB');
+    if (seq == 'A'){
+        if (!errorA) {
+            var div = document.createElement('div');
+            div.innerHTML = '<div class="error" id="errorA" style="display: inline-block;">Sekwencja A jest nieprawidłowa! Musi składać się wyłącznie ze znaków A,C,T,G i nie może być pusta</div>';
+            document.getElementById('seqADiv').appendChild(div);
+        }
+        else {
+            errorA.innerHTML = "Sekwencja A jest nieprawidłowa! Musi składać się wyłącznie ze znaków A,C,T,G i nie może być pusta";
+        }
     }
     else {
-        div.innerHTML = '<div class="error" id="break_penalty__error" style="display: inline-block;">Sekwencja B jest nieprawidłowa! Musi składać się wyłącznie ze znaków A,C,T,G i nie może być pusta</div>';
-        document.getElementById('seqBDiv').appendChild(div);
+        if (!errorB) {
+            var div = document.createElement('div');
+            div.innerHTML = '<div class="error" id="errorB" style="display: inline-block;">Sekwencja B jest nieprawidłowa! Musi składać się wyłącznie ze znaków A,C,T,G i nie może być pusta</div>';
+            document.getElementById('seqBDiv').appendChild(div);
+        }
+        else {
+            errorB.innerHTML = "Sekwencja B jest nieprawidłowa! Musi składać się wyłącznie ze znaków A,C,T,G i nie może być pusta";
+        }
     }
 }
 
-function removeWarning(input) {
-    document.getElementById('content').removeChild(input.parentNode);
+function removeWarning(seq) {
+    var div = document.createElement('div');
+    if (seq == 'A') {
+        document.getElementById('errorA').innerHTML= "";
+    }
+    else {
+        document.getElementById('errorB').innerHTML= "";
+    }
 }
